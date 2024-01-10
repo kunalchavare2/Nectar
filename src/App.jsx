87dd78883e3da9df7931e3a0dacb875b5040dc35
sushmalanke <sushmalanke31@gmail.com>
2024-01-10 16:58:21 +0530

    categoryItem

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,13 @@
 import "./App.css";
 import GlobalStyles from "./styles/globalStyles";
-import Theme from "./Theme/Theme";
-import Category from "./components/Atoms/Category/Category";
-import bakery from "./assets/images/categories/bakery.png";
-import rice from "./assets/images/categories/rice.png";
+import Theme from "./Theme/Theme"; 
 function App() {
   return (
     <>
       <Theme>
         <GlobalStyles />
       </Theme>
-      <Category
-        imgSrc={bakery}
-        text="bakery"
-        isCard={true}
-        backgroundColor ="rgba(247, 165, 147, 0.25)"
-      />
-      <Category
-        imgSrc={rice}
-        text="hello"
-        isCard={false}
-        backgroundColor="rgba(183, 223, 245, 0.25)"
-      />
+      
     </>
   );
 }
