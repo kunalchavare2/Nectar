8457134b742fefdf14b7c846829f61acaafae657
Kunal Chavare <kunalchavare2@gmail.com>
2024-01-10 21:29:40 +0530

    Updated products and categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import ProductCard from "./components/Molecules/ProductCard/ProductCard";
 import GlobalStyles from "./styles/globalStyles";
-import Theme from "./Theme/Theme"; 
+import Theme from "./Theme/Theme";
 function App() {
   return (
     <Theme>
       <GlobalStyles />
-      <ProductCard layout="favorurite" style={{ width: "300px" }} />
+      <ProductCard layout="favorurite" />
     </Theme>
   );
 }
