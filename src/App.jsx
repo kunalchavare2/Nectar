bf7e51ef54e70d7a524518f28c12e93aa23840dd
sushmalanke <sushmalanke31@gmail.com>
2024-01-11 16:05:24 +0530

    NavIcons

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import "./App.css";
+import React from "react";
+import "./App.css"; 
 import ProductCard from "./components/Molecules/ProductCard/ProductCard";
 import GlobalStyles from "./styles/globalStyles";
 import Theme from "./Theme/Theme";
+import NavIconsPage from "./components/Molecules/NavIconsPage/NavIconsPage";
 function App() {
   return (
-    <Theme>
+    <>
+     <Theme>
       <GlobalStyles />
       <ProductCard layout="favorurite" />
-    </Theme>
+    </Theme> 
+    <NavIconsPage/>
+    </>
+   
+
   );
 }
 
