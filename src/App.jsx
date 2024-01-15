808530f564f94ab6a9141442f863fbacfe3e5ebb
sushmalanke <sushmalanke31@gmail.com>
2024-01-15 12:05:53 +0530

    Page navigations icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
 import React from "react";
-import "./App.css"; 
+import "./App.css";
 import ProductCard from "./components/Molecules/ProductCard/ProductCard";
 import GlobalStyles from "./styles/globalStyles";
-import Theme from "./Theme/Theme";
-import NavIconsPage from "./components/Molecules/NavIconsPage/NavIconsPage"; 
+import Theme from "./Theme/Theme";  
+import ProductListPage from "./pages/ProductListPage/ProductListPage";
 function App() {
   return (
     <>
-     <Theme>
-      <GlobalStyles />
-      <ProductCard
-        layout="favorurite"
-        style={{ width: "300px" }}
-        quantityHandler={(v) => {
-          console.log(v);
-        }}
-      />
-    </Theme> 
-    <NavIconsPage/> 
-    </>
+      <Theme>
+        <GlobalStyles />
+        <ProductCard
+          layout="favorurite"
+          style={{ width: "300px" }}
+          quantityHandler={(v) => {
+            console.log(v);
+          }}
+        />
+        <ProductListPage/>
+      </Theme> 
    
-
+    </>
   );
 }
 
---
+++ src/pages/ProductListPage/ProductListPage.styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+export const Specifiedproductlist =styled.div`
+display: grid;
+grid-template-columns:1fr 1fr;
+gap:1rem;
+`;
+export const PageNAv =styled.div`
+display: flex;
+justify-content:space-between;
+align-items:center;
+padding:2rem 0;
+`;
+export const Productpage =styled.div`
+padding:1rem;
+`;
